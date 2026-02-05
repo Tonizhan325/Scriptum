166019c7f54be48828880f5b106f369993c01cf2
enmares <enmares2000@gmail.com>
2026-02-05 20:40:26 +0100

    Intentando implementar la vista de catálago con libros + generos

--- Scriptum/Scriptum/Controllers/CatalogoController.cs
+++ Scriptum/Scriptum/Controllers/CatalogoController.cs
@@ -1,14 +1,33 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Scriptum.Data;
+using Scriptum.Models;
 
 namespace Scriptum.Controllers
 {
     //[Authorize(Roles = "Usuario")]
     public class CatalogoController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public CatalogoController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
         }
+
+        //public IActionResult Index()
+        //{
+        //    var vm = new CatalogoViewModel
+        //    {
+        //        Libros = _context.Libros
+        //                         .Include(l => l.Genero)
+        //                         .ToList(),
+
+        //        Generos = _context.Generos.ToList()
+        //    };
+
+        //    return View(vm);
+        //}
     }
 }
---
+++ Scriptum/Scriptum/Models/CatalogoViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Scriptum.Models
+{
+    public class CatalogoViewModel
+    {
+        public IEnumerable<Libro> Libros { get; set; }
+        public IEnumerable<Genero> Generos { get; set; }
+    }
+}
