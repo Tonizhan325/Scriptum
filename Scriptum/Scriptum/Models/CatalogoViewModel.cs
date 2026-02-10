1dd027d96c62ffc1942d73a4464a1906a0c6ef42
enmares <enmares2000@gmail.com>
2026-02-10 16:51:28 +0100

    Añadida paginación a catálogo

--- Scriptum/Scriptum/Models/CatalogoViewModel.cs
+++ Scriptum/Scriptum/Models/CatalogoViewModel.cs
@@ -4,5 +4,10 @@
     {
         public IEnumerable<Libro> Libros { get; set; } = Enumerable.Empty<Libro>();
         public IEnumerable<Genero> Generos { get; set; } = Enumerable.Empty<Genero>();
+
+        public int PaginaActual { get; set; }
+        public int TotalPaginas { get; set; }
+        public string FiltroBusqueda { get; set; }
+        public string? GeneroSeleccionado { get; set; }
     }
 }
