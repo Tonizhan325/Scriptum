4a411b0cfb8a75a0e0ad633522ee98a9db2844b3
Tonizhan325 <angelzhan666@gmail.com>
2026-02-10 16:50:31 +0100

    Mis datos usuarios implementado

--- Scriptum/Scriptum/Controllers/HomeController.cs
+++ Scriptum/Scriptum/Controllers/HomeController.cs
@@ -1,20 +1,35 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Scriptum.Models;
+using Scriptum.Data;
 
 namespace Scriptum.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            // Busca el empleado correspondiente al usuario actual. Si existe, activa la
+            // vista (View) y en caso contrario, se redirige para crear el empleado.
+            string? emailUsuario = User.Identity.Name;
+            Usuario? usuario = _context.Usuarios.Where(e => e.Email == emailUsuario)
+            .FirstOrDefault();
+            if (User.Identity.IsAuthenticated &&
+            User.IsInRole("Usuario") &&
+            usuario == null)
+            {
+                return RedirectToAction("Create", "MisDatos");
+            }
+
             return View();
         }
 
---
+++ Scriptum/Scriptum/Controllers/MisDatosController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Scriptum.Data;
+using Scriptum.Models;
+
+namespace Scriptum.Controllers
+{
+    [Authorize(Roles = "Usuario")]
+    public class MisDatosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public MisDatosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: MisDatos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        // POST: MisDatos/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Nombre,Contraseña,Email,fechaRegistro,Estado")] Usuario usuario)
+        {
+            // Asignar el Email del usuario actual
+            usuario.Email = User.Identity.Name;
+            if (ModelState.IsValid)
+            {
+                _context.Add(usuario);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+            return View(usuario);
+        }
+
+        // GET: MisDatos/Edit
+        public async Task<IActionResult> Edit()
+        {
+            // Se seleccionan los datos del usuario correspondiente al usuario actual
+            string? emailUsuario = User.Identity.Name;
+            Usuario? empleado = await _context.Usuarios
+            .Where(e => e.Email == emailUsuario)
+            .FirstOrDefaultAsync();
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+            return View(empleado);
+        }
+        // POST: MisDatos/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id,[Bind("Nombre,Contraseña,Email,fechaRegistro,Estado")] Usuario usuario)
+        {
+            if (id != usuario.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(usuario);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmpleadoExists(usuario.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index", "Home");
+            }
+            return View(usuario);
+        }
+        private bool EmpleadoExists(int id)
+        {
+            return (_context.Usuarios?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+    }
+}
